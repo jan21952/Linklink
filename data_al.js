45d29db3326a8d2fbc7cb83f953a2f4bb1dbd407
jan21952 <127497709+jan21952@users.noreply.github.com>
2026-06-08 15:52:44 +0800

    Add files via upload

---
+++ data_al.js
@@ -0,0 +1,88 @@
+﻿// data.js
+window.iChingData = [
+  {
+    "filename": "1140629",
+    "num": "001",
+    "guaName": "澤風大過之鼎",
+    "nature": "自占",
+    "category": "健康"
+  },
+  {
+    "filename": "1140630-1",
+    "num": "002",
+    "guaName": "天火同人之旅",
+    "nature": "自占",
+    "category": "健康"
+  },
+  {
+    "filename": "1140630-2",
+    "num": "003",
+    "guaName": "離為火之大有",
+    "nature": "他占",
+    "category": "健康"
+  },
+  {
+    "filename": "1140701",
+    "num": "004",
+    "guaName": "地雷復之雷",
+    "nature": "他占",
+    "category": "官司"
+  },
+  {
+    "filename": "1140702",
+    "num": "005",
+    "guaName": "天風姤",
+    "nature": "自占",
+    "category": "健康"
+  },
+  {
+    "filename": "1140703",
+    "num": "006",
+    "guaName": "澤地萃之同人",
+    "nature": "他占",
+    "category": "官司"
+  },
+  {
+    "filename": "1140704",
+    "num": "007",
+    "guaName": "天地否之訟",
+    "nature": "他占",
+    "category": "官司"
+  },
+  {
+    "filename": "1140705-1",
+    "num": "008",
+    "guaName": "水天需之乾",
+    "nature": "自占",
+    "category": "運勢"
+  },
+  {
+    "filename": "1140705-2",
+    "num": "008",
+    "guaName": "水天需之乾",
+    "nature": "自占",
+    "category": "運勢"
+  },
+  {
+    "filename": "1140706",
+    "num": "009",
+    "guaName": "坤為地",
+    "nature": "自占",
+    "category": "運勢"
+  },
+  {
+    "filename": "1140707",
+    "num": "010",
+    "guaName": "兌為澤之歸妹",
+    "nature": "自占",
+    "category": "運勢"
+  },
+  {
+    "filename": "1140708",
+    "num": "011",
+    "guaName": "坤為地之晉",
+    "nature": "自占",
+    "category": "運勢"
+  }
+  // 後續資料請繼續在此陣列中新增...
+];
