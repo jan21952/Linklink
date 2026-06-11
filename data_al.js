803c0e2ffb8cbde5633392e6f0ce4af1e6d7ab9d
jan21952 <127497709+jan21952@users.noreply.github.com>
2026-06-11 15:20:42 +0800

    Add files via upload

--- data_al.js
+++ data_al.js
@@ -1,88 +1,770 @@
-﻿// data.js
-window.iChingData = [
+﻿window.iChingData = [
   {
     "filename": "1140629",
-    "num": "001",
+    "num": "1",
     "guaName": "澤風大過之鼎",
     "nature": "自占",
     "category": "健康"
+    "chapter": "010.元神、忌神、衰旺章"
   },
   {
     "filename": "1140630-1",
-    "num": "002",
+    "num": "2",
     "guaName": "天火同人之旅",
     "nature": "自占",
     "category": "健康"
+    "chapter": "010.元神、忌神、衰旺章"
   },
   {
     "filename": "1140630-2",
-    "num": "003",
+    "num": "3",
     "guaName": "離為火之大有",
     "nature": "他占",
     "category": "健康"
+    "chapter": "010.元神、忌神、衰旺章"
   },
   {
     "filename": "1140701",
-    "num": "004",
+    "num": "4",
     "guaName": "地雷復之雷",
     "nature": "他占",
     "category": "官司"
+    "chapter": "011.五行相生章"
   },
   {
     "filename": "1140702",
-    "num": "005",
+    "num": "5",
     "guaName": "天風姤",
     "nature": "自占",
     "category": "健康"
+    "chapter": "011.五行相生章"
   },
   {
     "filename": "1140703",
-    "num": "006",
+    "num": "6",
     "guaName": "澤地萃之同人",
     "nature": "他占",
     "category": "官司"
+    "chapter": "012.五行相剋章"
   },
   {
     "filename": "1140704",
-    "num": "007",
+    "num": "7",
     "guaName": "天地否之訟",
     "nature": "他占",
     "category": "官司"
+    "chapter": "012.五行相剋章"
   },
   {
     "filename": "1140705-1",
-    "num": "008",
+    "num": "8",
     "guaName": "水天需之乾",
     "nature": "自占",
     "category": "運勢"
+    "chapter": "012.五行相剋章"
   },
   {
     "filename": "1140705-2",
-    "num": "008",
+    "num": "8",
     "guaName": "水天需之乾",
     "nature": "自占",
     "category": "運勢"
+    "chapter": "012.五行相剋章"
   },
   {
     "filename": "1140706",
-    "num": "009",
+    "num": "9",
     "guaName": "坤為地",
     "nature": "自占",
     "category": "運勢"
+    "chapter": "014.動靜生克章"
   },
   {
     "filename": "1140707",
-    "num": "010",
+    "num": "10",
     "guaName": "兌為澤之歸妹",
     "nature": "自占",
     "category": "運勢"
+    "chapter": "014.動靜生克章"
   },
   {
     "filename": "1140708",
-    "num": "011",
+    "num": "11",
     "guaName": "坤為地之晉",
     "nature": "自占",
     "category": "運勢"
+    "chapter": "015.動變生克衝合章"
+  },
+  {
+    "filename": "1140709",
+    "num": "12",
+    "guaName": "火天大有",
+    "nature": "自占",
+    "category": "財運"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140710",
+    "num": "13",
+    "guaName": "山風蠱之蒙",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140711",
+    "num": "14",
+    "guaName": "艮為山之頤",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140712",
+    "num": "15",
+    "guaName": "澤水困之恆",
+    "nature": "他占",
+    "category": "官司"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140713",
+    "num": "16",
+    "guaName": "艮為山之明夷",
+    "nature": "自占",
+    "category": "財運"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140714",
+    "num": "17",
+    "guaName": "火地晉",
+    "nature": "他占",
+    "category": "健康"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140715",
+    "num": "18",
+    "guaName": "天澤履之中孚",
+    "nature": "他占",
+    "category": "健康"
+    "chapter": "017.月將章"
+  },
+  {
+    "filename": "1140716",
+    "num": "19",
+    "guaName": "天山遯之姤",
+    "nature": "自占",
+    "category": "健康"
+    "chapter": "018.日辰章"
+  },
+  {
+    "filename": "1140717",
+    "num": "20",
+    "guaName": "澤天夬履",
+    "nature": "他占",
+    "category": "運勢"
+    "chapter": "018.日辰章"
+  },
+  {
+    "filename": "1140718",
+    "num": "21",
+    "guaName": "山地剝之觀",
+    "nature": "他占",
+    "category": "健康"
+    "chapter": "019.六神章"
+  },
+  {
+    "filename": "1140719",
+    "num": "22",
+    "guaName": "水雷屯之震",
+    "nature": "他占",
+    "category": "健康"
+    "chapter": "019.六神章"
+  },
+  {
+    "filename": "1140720",
+    "num": "23",
+    "guaName": "風地觀之否",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "019.六神章"
+  },
+  {
+    "filename": "1140721",
+    "num": "24",
+    "guaName": "巽為風之渙",
+    "nature": "自占",
+    "category": "官司"
+    "chapter": "019.六神章"
+  },
+  {
+    "filename": "1140722",
+    "num": "25",
+    "guaName": "坎為水之困",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140723",
+    "num": "26",
+    "guaName": "地天泰",
+    "nature": "自占",
+    "category": "官司"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140724",
+    "num": "27",
+    "guaName": "地火明夷之小過",
+    "nature": "自占",
+    "category": "運勢"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140725",
+    "num": "28",
+    "guaName": "離為火之旅",
+    "nature": "自占",
+    "category": "官司"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140726",
+    "num": "29",
+    "guaName": "澤水困之節",
+    "nature": "自占",
+    "category": "運勢"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140727",
+    "num": "30",
+    "guaName": "山地剝",
+    "nature": "他占",
+    "category": "健康"
+    "chapter": "020.六合章"
+  },
+  {
+    "filename": "1140728",
+    "num": "31",
+    "guaName": "離為火之坤",
+    "nature": "自占",
+    "category": "官司"
+    "chapter": "021.三合章"
+  },
+  {
+    "filename": "1140729",
+    "num": "32",
+    "guaName": "乾為天之需",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "021.三合章"
+  },
+  {
+    "filename": "1140730",
+    "num": "33",
+    "guaName": "乾為天之小畜",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "021.三合章"
+  },
+  {
+    "filename": "1140731",
+    "num": "34",
+    "guaName": "澤地萃之革",
+    "nature": "自占",
+    "category": "事業"
+    "chapter": "021.三合章"
+  },
+  {
+    "filename": "1140801",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140802",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140803",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140804",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140805",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140806",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140807",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140808",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140809",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140810",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140811",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140812",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140813-1",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140814",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140815",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140816",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140817",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140818",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140819",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140820",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140821",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140822",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140823",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140824-1",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140825",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140826",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140827",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140828",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140829",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140830",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140831",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140901",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140902",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140903",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140904",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140905",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140906",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140907",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140908",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140909",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140910",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140911",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140912",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140913",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140914",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140915",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140916",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140917",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140918",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140919",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140920",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140921",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140922",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140923",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140924",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140925",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140926",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140927",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140928",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140929",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
+  },
+  {
+    "filename": "1140930",
+    "num": "",
+    "guaName": "",
+    "nature": "",
+    "category": ""
+    "chapter": ""
   }
-  // 後續資料請繼續在此陣列中新增...
 ];
